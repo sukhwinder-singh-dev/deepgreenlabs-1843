b09dafd1d1b74569bd90d02e2dbb41b2fa6cc032
Mario Reder <mario.reder@pm.me>
2025-07-22 11:29:29 +0200

    fix portfolio page current item

--- app/routes/portfolio.tsx
+++ app/routes/portfolio.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import { Outlet, useLocation } from "@remix-run/react";
 import { PortfolioLayoutWidget } from "@orderly.network/portfolio";
 import { useOrderlyConfig } from "@/utils/config";
@@ -11,13 +10,6 @@ export default function PortfolioLayout() {
   const { onRouteChange } = useNav();
   const config = useOrderlyConfig();
 
-  const currentPath = useMemo(() => {
-    if (pathname.endsWith("/portfolio")) return "/portfolio";
-    if (pathname.endsWith("/portfolio/fee")) return "/portfolio/feeTier";
-    if (pathname.endsWith("/portfolio/api-key")) return "/portfolio/apiKey";
-    return pathname;
-  }, [pathname]);
-
   return (
     <PortfolioLayoutWidget
       footerProps={config.scaffold.footerProps}
@@ -29,7 +21,7 @@ export default function PortfolioLayout() {
         onRouteChange,
       }}
       leftSideProps={{
-        current: currentPath,
+        current: pathname,
       }}
       bottomNavProps={config.scaffold.bottomNavProps}
     >
