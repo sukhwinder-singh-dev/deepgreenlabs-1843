cb56ae984f9369efbb1b80e8761348e777fa039b
Mario Reder <mario.reder@pm.me>
2025-08-05 15:13:31 +0200

    feat: markets exchanges comparison name and logo

--- app/routes/markets._index.tsx
+++ app/routes/markets._index.tsx
@@ -10,5 +10,18 @@ export const meta: MetaFunction = () => {
 };
 
 export default function MarketsPage() {
-  return <MarketsHomePage />;
+  return (
+    <MarketsHomePage
+      comparisonProps={{
+        exchangesIconSrc:
+          import.meta.env.VITE_HAS_SECONDARY_LOGO === "true"
+            ? "/logo-secondary.webp"
+            : undefined,
+        exchangesName:
+          import.meta.env.VITE_ORDERLY_BROKER_NAME
+            ? import.meta.env.VITE_ORDERLY_BROKER_NAME
+            : undefined,
+      }}
+    />
+  );
 }
