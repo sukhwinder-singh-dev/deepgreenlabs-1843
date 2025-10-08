8d33b7ff27aa1e0e16dedd8bbae04afd07242d1a
Mario Reder <mario.reder@pm.me>
2025-10-08 11:05:06 +0200

    feat: pwa

--- app/main.tsx
+++ app/main.tsx
@@ -128,3 +128,16 @@ loadRuntimeConfig().then(() => {
   );
 });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register(withBasePath('/sw.js'))
+      .then((registration) => {
+        console.log('SW registered:', registration);
+      })
+      .catch((error) => {
+        console.log('SW registration failed:', error);
+      });
+  });
+}
+
